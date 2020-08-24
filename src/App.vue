232377df7792b4ee49897390e00ada0024912177
chenglla <1225883052@qq.com>
2020-08-24 15:07:45 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default  {
+  name:  'App'
+}
+</script>
+<style>
+  /*div {*/
+  /*  height: 100%;*/
+  /*  width: 100%;*/
+  /*}*/
+  body {
+    height: 100%;
+  }
+  #app {
+    height: 100%;
+  }
+</style>
