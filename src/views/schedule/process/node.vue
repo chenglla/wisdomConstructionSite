f1f9a29c8e708ec49c44cadc809740f2b0f96a5c
Anmengfei <mf_0604@163.com>
2021-03-25 14:36:15 +0800

    fix bugs

--- src/views/schedule/process/node.vue
+++ src/views/schedule/process/node.vue
@@ -1085,22 +1085,40 @@ export default {
       this.title = '修改节点'
     },
     handleDel(row) {
+
       console.log("row", row)
-      delNode(row.id).then((res) => {
-        if(res.code === 200) {
-          this.$message({
-            type: 'success',
-            message: '删除成功！'
+      this.$confirm('此操作将永久删除, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning',
+          center: true
+        }).then(() => {
+          delNode(row.id).then((res) => {
+            if(res.code === 200) {
+              this.$message({
+                type: 'success',
+                message: '删除成功！'
+              })
+              this.getNodeList();
+              
+            } else {
+              this.$message({
+                type: 'error',
+                message: '删除失败！'
+              })
+            }
           })
-          this.getNodeList();
-          
-        } else {
+              
+        }).catch(() => {
           this.$message({
-            type: 'error',
-            message: '删除失败！'
-          })
-        }
-      })
+            type: 'info',
+            message: '已取消删除'
+          });
+        });
+
+
+      
+      
     },
     
 
--- src/views/schedule/process/task.vue
+++ src/views/schedule/process/task.vue
@@ -579,21 +579,37 @@ export default {
       })
     },
     handleDel(row) {
-      delTask(row.id).then((res) => {
-        if(res.code === 200) {
-          this.$message({
-            type: 'success',
-            message: '删除成功！'
+
+      this.$confirm('此操作将永久删除, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning',
+          center: true
+        }).then(() => {
+          delTask(row.id).then((res) => {
+            if(res.code === 200) {
+              this.$message({
+                type: 'success',
+                message: '删除成功！'
+              })
+              this.getTaskList();
+              this.$emit('updateTree','updateTree');
+            } else {
+              this.$message({
+                type: 'error',
+                message: '删除失败！'
+              })
+            }
           })
-          this.getTaskList();
-          this.$emit('updateTree','updateTree');
-        } else {
+          
+        }).catch(() => {
           this.$message({
-            type: 'error',
-            message: '删除失败！'
-          })
-        }
-      })
+            type: 'info',
+            message: '已取消删除'
+          });
+        });
+
+      
     },
     handleUpdate(row) {
         console.log("chakan",row)
